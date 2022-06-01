73845a0e6917f6ce0f97a0a33f7efdd483cd100e
Alejo Flores Lucey <alejo@misterflores.com>
2022-06-01 18:46:56 -0300

    Changed primes print to please Horacio

--- Userland/SampleCodeModule/primes.c
+++ Userland/SampleCodeModule/primes.c
@@ -1,17 +1,33 @@
 #include <syscalls.h>
 #include <libc.h>
 
+static char prime_no[] = "Prime No ";
+static int prime_no_len = 9;
+static char equal[] = " = ";
+static int equal_len = 3;
+
 int get_next_prime(int last_prime);
 
 void primes(unsigned int fd) {
     int last_prime = 1;
+    int counter = 1;
     while (1) {
         last_prime = get_next_prime(last_prime);
         int length = num_length(last_prime);
         char str[length + 1];
         num_to_str(last_prime, str, length);
+
+        int counter_length = num_length(counter);
+        char counter_str[counter_length + 1];
+        num_to_str(counter, counter_str, counter_length + 1);
+
+        write(fd, prime_no, prime_no_len);
+        write(fd, counter_str, counter_length);
+        write(fd, equal, equal_len);
         write(fd, str, length + 1);
-        write(fd, " ", 1);
+        write(fd, "\n", 1);
+
+        counter++;
         // sleep(1);
     }
 }
