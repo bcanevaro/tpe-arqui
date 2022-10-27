88af3c560f42e225d720e6e73742827303f499f0
Juan Segundo Arnaude <juansarnaude@gmail.com>
2022-10-27 20:07:38 -0300

    JUEGUITO
    Faltan:
    -El empate,
    -Los bordes,
    -El sonidito,
    -La copa a Riber

--- Userland/SampleCodeModule/play.c
+++ Userland/SampleCodeModule/play.c
@@ -1,48 +1,19 @@
 #include <syscalls.h>
 #include <libc.h>
+#include <play.h>
 
-#define DOWN    0
-#define UP      1
-#define LEFT    2
-#define RIGHT   3
-
-typedef int color;
-
-#define BLACK 0x0
-#define BLUE 0x1
-#define GREEN 0x2
-#define CYAN 0x3
-#define RED 0x4
-#define MAGENTA 0x5
-#define BROWN 0x6
-#define L_GRAY 0x7
-#define GRAY 0x8
-#define L_BLUE 0x9
-#define L_GREEN 0xA
-#define L_CYAN 0xB
-#define L_RED 0xC
-#define L_MAGENTA 0xD
-#define YELLOW 0xE
-#define WHITE 0xF
-
-#define WIDTH 80
-#define HEIGHT 25
 static uint8_t * currentPlayer1Pos = (uint8_t*)0xB8000+1960; // cambiarlo
 static uint8_t * currentPlayer2Pos = (uint8_t*)0xB8000+2040; //cambiarlo
 static uint32_t playersPaintedPixels[HEIGHT * WIDTH] = {0};//Array que nos sirve para guardar el recorrido de los jugadores
 
-
-
-int drawMovement(char c, int player);
-//int checkCollision();
 /**
  *         0 if no player crashed
  *         1 if Colision was caused by player 1
  *         2 if Colision was caused by player 2
  *         3 if both crashed
  */
+static uint32_t crash = 0;
 
-uint32_t crash = 0;
 //80 * 12 = 960
 //960 * 2 = 1920
 //20 * 2 = 40
@@ -55,8 +26,6 @@ void play(unsigned int fd){
     char lastLetter1 = 's';
     char lastLetter2 = 'k';
     clear_screen();
-
-
 
     while(1){
         ret = read(1, letter, 1);
@@ -71,26 +40,49 @@ void play(unsigned int fd){
                 lastLetter1 = letter[0];
             }
             else if( letter[0]=='j' || letter[0]=='k' || letter[0]=='l' || letter[0]=='i'){
-                drawMovement(letter[0],2);
                 drawMovement(lastLetter1,1);
+                drawMovement(letter[0],2);
                 lastLetter2 = letter[0];
             }
             ret = 0;
 
-
-            if(crash==1){
-                break;
-            }
+        }if(crash!=0){
+            break;
         }
         sleepMiliseconds(65);
     }
+
+    clear_screen();
+
+    if(crash==1){
+        char str[12] = "Gano el azul";
+        write( fd, str , 12 );
+    }
+    else if(crash==2){
+        char str[12] = "Gano el rojo";
+        write( fd, str , 12 );
+    }
+    else if (crash==3){
+        char str[6] = "Empate";
+        write( fd, str , 6 );
+    }
+
+    for( int i=0 ; i<=(HEIGHT*WIDTH) ; i++ ){
+        playersPaintedPixels[i]=0;
+    }
+    currentPlayer1Pos = (uint8_t*)0xB8000+1960;
+    currentPlayer2Pos = (uint8_t*)0xB8000+2040;
+
+    sleepMiliseconds(1000);
+    clear_screen();
 }
 
 int drawMovement(char c, int player){
-	if(player==1){
+	if(player==1) {
         color background = RED;
         color foreground = RED;
-		if(c=='d' || c=='D'){
+
+        if(c=='d' || c=='D'){
             currentPlayer1Pos += 2;
             move(1,RIGHT,currentPlayer1Pos,foreground,background);
 		}
@@ -106,14 +98,13 @@ int drawMovement(char c, int player){
             currentPlayer1Pos += 2 * WIDTH;
 			move(1,DOWN,currentPlayer1Pos,foreground,background);
 		}
-        //checkCollision();
-        playersPaintedPixels[((uint32_t)currentPlayer1Pos - 0xB8000)/2] = 1;
-
+        checkCollision(1);
+        playersPaintedPixels[((uint32_t) currentPlayer1Pos - 0xB8000) / 2] = 1;
 	}
 	else{
         color background = BLUE;
         color foreground = BLUE;
-		playersPaintedPixels[((uint32_t)currentPlayer2Pos - 0xB8000)/2] = 1;
+
 		if(c=='l' || c=='L'){
             currentPlayer2Pos += 2;
 			move(1,RIGHT,currentPlayer2Pos,foreground,background);
@@ -130,23 +121,21 @@ int drawMovement(char c, int player){
             currentPlayer2Pos += 2 * WIDTH;
 			move(1,DOWN,currentPlayer2Pos,foreground,background);
 		}
+        checkCollision(2);
+        playersPaintedPixels[((uint32_t)currentPlayer2Pos - 0xB8000)/2] = 1;
 	}
 }
-/*
-cleanPlayersPaintedPixels(){
-    for( int i=0 ; i<=(HEIGHT*WIDTH) ; i++ ){
-        playersPaintedPixels[i]=0;
+
+
+int checkCollision(int player){
+    if(player==1){
+        if(playersPaintedPixels[((uint32_t)currentPlayer1Pos - 0xB8000)/2]==1){
+            crash = 1;
+        }
+    }
+    if(player==2){
+        if(playersPaintedPixels[((uint32_t)currentPlayer2Pos - 0xB8000)/2]==1){
+            crash += 2;
+        }
     }
 }
-
-int checkCollision(){
-    if(playersPaintedPixels[((uint32_t)currentPlayer1Pos - 0xB8000)/2]==1){
-        crash = 1;
-    }
-    if(playersPaintedPixels[((uint32_t)currentPlayer2Pos - 0xB8000)/2]==1){
-        crash += 2;
-    }
-    if(crash!=0){
-        cleanPlayersPaintedPixels();
-    }
-}*/
